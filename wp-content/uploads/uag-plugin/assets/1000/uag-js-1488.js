8767adcf50e27a844b319e5b0fad92e26bf07f43
Issam Ou <108191119+issam-out@users.noreply.github.com>
2025-02-08 17:36:27 +0000

    Add files via upload

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1488.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener('load', function() {
+        UAGBButtonChild.init('.uagb-block-cc514627');
+    });
+    window.addEventListener('load', function() {
+        UAGBButtonChild.init('.uagb-block-8bb4b826');
+    });
+});
